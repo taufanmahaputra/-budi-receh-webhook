f6a77b89824ed4d569317edd3d13f91e4a40b3aa
taufanmahaputra <hacker.indo62@gmail.com>
2018-04-21 17:01:32 +0700

    add log

---
+++ app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+require('dotenv').load();
+
+const config = {
+	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+	channelSecret: process.env.CHANNEL_SECRET
+}
+
+// Initial
+
+const app = express();
+const client = new line.Client(config);
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+console.log(port);
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
