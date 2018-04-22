8be1b38bdc9087333a32aaf39ae69cdad0557ffe
taufanmahaputra <hacker.indo62@gmail.com>
2018-04-22 14:44:16 +0700

    add initial mulai, pass, stop, and answer functionality

--- app.js
+++ app.js
@@ -57,39 +57,138 @@ function handleEvent(event) {
 
   // create a response text message
   let input = event.message.text.toLowerCase();
+
+	// Get question
+	var qa_index = Math.floor(Math.random()*soal.random.length);
+	var question_answer = soal.random[qa_index];
+
   if (input === 'nama')
 		echo.text = `Hai, ${profile.displayName}`;
 	else if (input === '/mulai') {
-		// Get question
-		const qa_index = Math.floor(Math.random()*soal.random.length);
-		const question_answer = soal.random[qa_index];
-		
-		var newGame = new Game({
-			groupId: event.source.groupId,
-			question: question_answer.question,
-		  answer: question_answer.answer,
-		  idx: qa_index,
-		  onGoing: true
-		});
-		
-		newGame.save((err, result) => {
+		Game.findOne({groupId: event.source.groupId}, (err, game) => {
 			if (err)
 				console.log(err);
 
-			console.log(result);
-		});
+			if (!game) {
+				var newGame = new Game({
+					groupId: event.source.groupId,
+					question: question_answer.question,
+				  answer: question_answer.answer,
+				  idx: qa_index,
+				  onGoing: true
+				});
+				
+				newGame.save((err, result) => {
+					if (err)
+						console.log(err);
 
-		echo.text = 'Game telah dimulai. Pertanyaan: \n';
-		echo.text += question_answer.question;
+					console.log(result);
+				});
+
+				echo.text = 'Game dimulai. Pertanyaan: \n';
+				echo.text += question_answer.question;
+			} 
+			else {
+				if (game.onGoing)
+					echo.text = 'Game sudah dimulai.';
+				else {
+					game.onGoing = true;
+					
+					while (qa_index == game.idx) {
+						qa_index = Math.floor(Math.random()*soal.random.length);
+					}
+
+					question_answer = soal.random[qa_index];
+
+					game.question = question_answer.question;
+					game.answer = question_answer.answer;
+					game.idx = qa_index;
+
+					echo.text = 'Game dimulai. Pertanyaan: \n';
+					echo.text += question_answer.question;
+				}
+			}
+		});
 	}
 	else if (input === '/pass') {
+		Game.findOne({groupId: event.source.groupId}, (err, game) => {
+			if (err)
+				console.log(err);
 
+			if (!game) {
+				echo.text = 'Game belum dimulai. Silahkan memulai permainan, sekararang!';
+			} 
+			else {
+				while (qa_index == game.idx) {
+					qa_index = Math.floor(Math.random()*soal.random.length);
+				}
+
+				question_answer = soal.random[qa_index];
+
+				game.question = question_answer.question;
+				game.answer = question_answer.answer;
+				game.idx = qa_index;
+				
+				game.save((err, result) => {
+					if (err)
+						console.log(err);
+
+					console.log(result);
+				});
+
+				echo.text = 'Payah! Pertanyaan baru: \n';
+				echo.text += question_answer.question;
+			}
+		});
 	}
-	else if (input === '/bye') {
+	else if (input === '/stop') {
+		Game.findOne({groupId: event.source.groupId}, (err, game) => {
+			if (err)
+				console.log(err);
 
+			if (!game) {
+				echo.text = 'Game belum dimulai. Silahkan memulai permainan, sekararang!';
+			} 
+			else {
+				game.onGoing = false;
+				game.save((err, result) => {
+					if (err)
+						console.log(err);
+
+					console.log(result);
+				});
+
+				echo.text = 'Sampai jumpa lagi!';
+			}
+		});
 	}
 	else {
-		echo.text = event.message.text
+		Game.findOne({groupId: event.source.groupId}, (err, game) => {
+			if (err)
+				console.log(err);
+
+			if (!game) {
+				echo.text = 'Game belum dimulai. Silahkan memulai permainan, sekararang!';
+			} 
+			else {
+				if (game.onGoing) {
+					if (input === game.answer) {
+						echo.text = 'Kamu berhasil nebak! Ketik /mulai untuk memulai kembali';
+
+						game.onGoing = false;
+						game.save((err, result) => {
+							if (err)
+								console.log(err);
+
+							console.log(result);
+						});
+					}
+				}
+				else {
+					echo.text = 'Game belum dimulai. Silahkan memulai permainan, sekararang!';
+				}
+			}
+		});
 	}
 
   // use reply API
